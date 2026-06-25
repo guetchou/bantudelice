76f99ce79e358748a2f078e75d5fd8dd8b783aab
guetchou <140221073+guetchou@users.noreply.github.com>
2026-06-25 14:43:27 +0100

    chore(realtime): restore untouched configuration comments

--- config/app.php
+++ config/app.php
@@ -75,8 +75,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | The application locale determines the default locale that will be used
-    | by the translation service provider. You are free to add as many
-    | languages as your application supports.
+    | by the translation service provider. You are free to set this value
+    | to any of the locales which will be supported by the application.
     |
     */
 
@@ -89,7 +89,7 @@ return [
     |
     | The fallback locale determines the locale to use when the current one
     | is not available. You may change the value to correspond to any of
-    | the language folders that are provided by your application.
+    | the language folders that are provided through your application.
     |
     */
 
@@ -101,8 +101,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | This locale will be used by the Faker PHP library when generating fake
-    | data for your database seeds. For example, this will determine the
-    | telephone number format, street address and more.
+    | data for your database seeds. For example, this will be used to get
+    | localized telephone numbers, street address information and more.
     |
     */
 
