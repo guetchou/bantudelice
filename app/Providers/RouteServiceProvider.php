d6e3724189158dfd7d79f19daa1a5ba383adf2d8
guetchou <140221073+guetchou@users.noreply.github.com>
2026-06-25 06:00:43 +0100

    Preserve Food route maps while loading Kende routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,9 +19,12 @@ class RouteServiceProvider extends ServiceProvider
     public function map()
     {
         $this->mapApiRoutes();
+        $this->mapApiV1FoodDriverRoutes();
         $this->mapWebRoutes();
         $this->mapKendeRoutes();
         $this->mapTrackingRoutes();
+        $this->mapAdminCashRoutes();
+        $this->mapAdminReportRoutes();
     }
 
     protected function mapWebRoutes()
@@ -44,11 +47,30 @@ class RouteServiceProvider extends ServiceProvider
              ->group(base_path('routes/tracking.php'));
     }
 
+    protected function mapAdminCashRoutes()
+    {
+        Route::middleware('web')
+             ->group(base_path('routes/admin-cash.php'));
+    }
+
+    protected function mapAdminReportRoutes()
+    {
+        Route::middleware('web')
+             ->group(base_path('routes/admin-reports.php'));
+    }
+
     protected function mapApiRoutes()
     {
         Route::prefix('api')
              ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
+    }
+
+    protected function mapApiV1FoodDriverRoutes()
+    {
+        Route::prefix('api/v1')
+             ->middleware('api')
+             ->group(base_path('routes/api-v1-food-driver.php'));
     }
 }
