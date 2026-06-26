356dd8b47751f2453bfc3b6a1175b4e80bb13562
guetchou <140221073+guetchou@users.noreply.github.com>
2026-06-26 17:38:44 +0100

    feat(gepay): ajouter la configuration de la passerelle

---
+++ config/gepay.php
@@ -0,0 +1,45 @@
+<?php
+
+$envBool = static fn (string $key, bool $default = false): bool => filter_var(
+    env($key, $default),
+    FILTER_VALIDATE_BOOLEAN
+);
+
+return [
+    'enabled' => $envBool('GEPAY_ENABLED', true),
+    'signature_tolerance_seconds' => (int) env('GEPAY_SIGNATURE_TOLERANCE_SECONDS', 300),
+    'default_provider' => env('GEPAY_DEFAULT_PROVIDER', 'mtn_momo'),
+
+    'providers' => [
+        'mtn_momo' => [
+            'enabled' => $envBool('GEPAY_MTN_ENABLED', false),
+            'environment' => env('GEPAY_MTN_ENVIRONMENT', 'sandbox'),
+            'target_environment' => env(
+                'GEPAY_MTN_TARGET_ENVIRONMENT',
+                env('GEPAY_MTN_ENVIRONMENT', 'sandbox') === 'sandbox' ? 'sandbox' : 'mtncongo'
+            ),
+            'base_url' => [
+                'sandbox' => env('GEPAY_MTN_SANDBOX_URL', 'https://sandbox.momodeveloper.mtn.com'),
+                'production' => env('GEPAY_MTN_PRODUCTION_URL', 'https://proxy.momoapi.mtn.com'),
+            ],
+            'callback_url' => env('GEPAY_MTN_CALLBACK_URL'),
+            'collections' => [
+                'subscription_key' => env('GEPAY_MTN_COLLECTIONS_SUBSCRIPTION_KEY'),
+                'api_user' => env('GEPAY_MTN_COLLECTIONS_API_USER'),
+                'api_key' => env('GEPAY_MTN_COLLECTIONS_API_KEY'),
+            ],
+            'disbursements' => [
+                'subscription_key' => env('GEPAY_MTN_DISBURSEMENTS_SUBSCRIPTION_KEY'),
+                'api_user' => env('GEPAY_MTN_DISBURSEMENTS_API_USER'),
+                'api_key' => env('GEPAY_MTN_DISBURSEMENTS_API_KEY'),
+            ],
+            'remittances' => [
+                'enabled' => $envBool('GEPAY_MTN_REMITTANCES_ENABLED', false),
+                'subscription_key' => env('GEPAY_MTN_REMITTANCES_SUBSCRIPTION_KEY'),
+                'api_user' => env('GEPAY_MTN_REMITTANCES_API_USER'),
+                'api_key' => env('GEPAY_MTN_REMITTANCES_API_KEY'),
+                'token_path' => env('GEPAY_MTN_REMITTANCES_TOKEN_PATH'),
+            ],
+        ],
+    ],
+];
